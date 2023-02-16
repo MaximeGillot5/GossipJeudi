c930b8fa65215f2cf9ea38b2b85eabe632430fe4
MaximeGillot5 <mgillot56@icloud.com>
2023-02-16 18:11:54 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  resources :gossip
+  resources :user
+  resources :sessions, only: [:new, :create, :destroy]
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :city
+  post 'user/new'
+  #get 'static_pages/contact'
+  #get 'static_pages/team'
+  get 'contact', to: 'static_pages#contact'
+  get 'team', to: 'static_pages#team'
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'static_pages#home'
+end
